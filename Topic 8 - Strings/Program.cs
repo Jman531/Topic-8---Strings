f8a9dee3021c96a4013198f73bf28f70528134db
Jack Gallagher <339690067@GoTVDSB.ca>
2024-12-04 08:08:49 -0500

    Add project files.

---
+++ Topic 8 - Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_8___Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
